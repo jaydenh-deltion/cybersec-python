bfd1b5aa2b3f8a4c1fd9e298b05c45d79e736c3a
jaydenh-deltion <161732348+JaydenH04@users.noreply.github.com>
2025-10-20 19:17:15 +0200

    Create password gen.py

---
+++ password gen/password gen.py
@@ -0,0 +1,24 @@
+import string
+print("Python gebruikt string module vanaf:", string.__file__)
+
+
+
+import random # voor random keuzes van de tekens die er in komen 
+import string # voor meer opties dan die er standaard zijn
+
+def generate_password(length: int ): # functie om een wachtwoord te genereren
+    alphabet =string.ascii_letters + string.digits + string.punctuation # alle mogelijke tekens die er in kunnen komen
+    password = ''.join( random.choice(alphabet) for i in range(length)) # maakt een wachtwoord met de lengte die de gebruiker opgeeft
+    return password # geeft het wachtwoord terug
+
+length = int(input("hoe lang wil je het wachtwoord hebben? : ")) # vraag de gebruiker om de lengte van het wachtwoord
+
+if length <= 8: # controleer of de lengte minder dan of gelijk is aan 8
+    print("waarschuwing: een wachtwoord van minder dan 8 tekens in minder veilig .")
+
+else: 
+    print("je wachtwoord is veilig genoeg .")
+
+
+
+print(generate_password(length)) # print wachtwoord in de terminal 
